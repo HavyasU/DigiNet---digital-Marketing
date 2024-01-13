553b35954970598b9a9ad909232a9b51ad19f7c1
Havyas U <113080389+HavyasRai@users.noreply.github.com>
2024-01-13 10:35:02 +0530

    css update

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const backToTop = document.getElementById('back-to-top');
 window.onscroll = (e) => {
-    if(window.pageYOffset > 5)
-    {
-        backToTop.style.display = "flex"
+    if (window.pageYOffset > 5) {
+        backToTop.style.display = "flex";
     }
-    else
-    {
-        backToTop.style.display = "none"
+    else {
+        backToTop.style.display = "none";
     }
+};
+const menuIcon = document.getElementById('menu-icon');
+const closeIcon = document.getElementById('close-icon');
+menuIcon.onclick = (e) => {
+    menuIcon.parentElement.firstElementChild.classList.toggle('mobile-nav-active');
+    const currentIcon = menuIcon.firstElementChild.getAttribute('name')
+    const nextIcon = currentIcon=="menu" ? "x-circle" : "menu"
+    menuIcon.firstElementChild.setAttribute("name",nextIcon);
 };
